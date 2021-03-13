cbe4c50e4e5aa97dccef6d2fd78b711b0061ddda
Portable <53180119+Helicraft08@users.noreply.github.com>
2021-03-13 12:07:46 +0100

    Create scatter3d.py

---
+++ scatter3d.py
@@ -0,0 +1,46 @@
+'''
+==============
+3D scatterplot
+==============
+
+Demonstration of a basic scatterplot in 3D.
+'''
+
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mysql.connector
+
+mydb = mysql.connector.connect(
+        host="localhost",
+        port="52000",
+        user="utilisateur",
+        password="root",
+        database="elite",
+        auth_plugin='mysql_native_password'
+        )
+
+fig = plt.figure()
+
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+
+df_mysql = pd.read_sql('select * from elite.System;', con=mydb)  
+df2_mysql = pd.DataFrame(df_mysql)  
+print ('loaded dataframe from MySQL. records:', len(df2_mysql))
+mydb.close()
+
+print(df2_mysql.sample())
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(df2_mysql["longitude"],df2_mysql[["latitude"]],df2_mysql[["altitude"]])
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
